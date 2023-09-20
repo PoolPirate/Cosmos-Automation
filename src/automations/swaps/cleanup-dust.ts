2dca77348bc99245d53d67557badb3c1c436b20e
Playwo <eliaswolf1609@gmail.com>
2023-09-20 12:48:23 +0200

    Always send to owner

--- src/automations/swaps/cleanup-dust.ts
+++ src/automations/swaps/cleanup-dust.ts
@@ -60,56 +60,62 @@ export async function runCleanupDustAsync(chain: Chain) {
             });
 
             await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
 
-            const osmoBalance = (await getBalance(chain, 'uosmo')) - 10000000;
-            await executeMultiple(
-                chain,
-                [
-                    {
-                        contractAddress:
-                            'osmo1fy547nr4ewfc38z73ghr6x62p7eguuupm66xwk8v8rjnjyeyxdqs6gdqx7',
-                        msg: {
-                            swap: {
-                                input_coin: {
-                                    denom: 'uosmo',
-                                    amount: `${osmoBalance}`,
-                                },
-                                output_denom:
-                                    'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
-                                slippage: {
-                                    twap: {
-                                        slippage_percentage: '0.5',
-                                        window_seconds: 5,
-                                    },
-                                },
-                            },
-                        },
-                        funds: [
-                            {
+        const osmoBalance = (await getBalance(chain, 'uosmo')) - 10000000;
+
+        if (osmoBalance < 0) {
+            return;
+        }
+
+        await executeMultiple(
+            chain,
+            [
+                {
+                    contractAddress:
+                        'osmo1fy547nr4ewfc38z73ghr6x62p7eguuupm66xwk8v8rjnjyeyxdqs6gdqx7',
+                    msg: {
+                        swap: {
+                            input_coin: {
                                 denom: 'uosmo',
                                 amount: `${osmoBalance}`,
                             },
-                        ],
+                            output_denom:
+                                'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
+                            slippage: {
+                                twap: {
+                                    slippage_percentage: '0.5',
+                                    window_seconds: 5,
+                                },
+                            },
+                        },
                     },
-                ],
-                {
-                    simulateAsPrimary: true,
-                    gasMultiplicator: 1.2,
+                    funds: [
+                        {
+                            denom: 'uosmo',
+                            amount: `${osmoBalance}`,
+                        },
+                    ],
                 },
-            );
+            ],
+            {
+                simulateAsPrimary: true,
+                gasMultiplicator: 1.2,
+            },
+        );
 
-            await new Promise((resolve) => setTimeout(resolve, 5000));
-            const usdcBalance = await getBalance(
-                chain,
-                'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
-            );
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const usdcBalance = await getBalance(
+            chain,
+            'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
+        );
 
-            await sendToOwner(
-                chain,
-                'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
-                usdcBalance,
-            );
-        }
+        await sendToOwner(
+            chain,
+            'ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858',
+            usdcBalance,
+        );
+        //}
     } catch (error) {
         console.error('Cleanup failed: ' + error);
     }
